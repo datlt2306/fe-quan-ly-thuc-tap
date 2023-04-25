00ba9da9b8ada272979734bc2691419a753c7143
quanghiep03198 <quanghiep03198@gmail.com>
2023-04-25 21:29:08 +0700

    @PeoScouser98 - Tailwind styled component

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,33 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+const colors = require('tailwindcss/colors');
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: [],
+	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {
+		colors: {
+			primary: '#057cc3',
+			'primary-active': '#0d51a1',
+			error: '#f87272',
+			success: '#4eb849',
+			disabled: '#9ca3af',
+			...colors,
+		},
+		screens: {
+			sm: {
+				min: '375px',
+				max: '767px',
+			},
+			md: {
+				min: '768px',
+				max: '1365px',
+			},
+			lg: {
+				min: '1366px',
+				max: '1920px',
+			},
+			xl: { min: '1921px' },
+		},
+		extends: {
 			fontFamily: {
 				sans: ['Inter var', ...defaultTheme.fontFamily.sans],
 			},
--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,26 @@
-import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
-import path from "path"
+import path from 'path';
 
 export default defineConfig({
-    resolve: {
-        alias: [
-            {
-                find: "@",
-                replacement: path.resolve(__dirname, "src")
-            }
-        ]
-    },
-    plugins: [react()],
+	resolve: {
+		alias: [
+			{
+				find: '@',
+				replacement: path.resolve(__dirname, 'src'),
+			},
+		],
+	},
+	plugins: [
+		react({
+			babel: {
+				plugins: ['babel-plugin-macros', 'babel-plugin-styled-components'],
+			},
+		}),
+	],
 
-    server: {
-        port: 9988
-    }
-})
+	server: {
+		port: 9988,
+	},
+});
