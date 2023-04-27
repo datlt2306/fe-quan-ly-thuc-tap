d9c10467003815706580247f2fb8c19b5bcbe66a
quanghiep03198 <quanghiep03198@gmail.com>
2023-04-28 02:09:29 +0700

    (QLTT-4) update core form field control

--- src/Core/components/common/SelectFieldControl.jsx
+++ src/Core/components/common/SelectFieldControl.jsx
@@ -2,10 +2,15 @@ import { useId } from "react";
 import { Controller } from "react-hook-form";
 import tw from "twin.macro";
 
-const Select = tw.select`mt-2 block w-full rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-1 focus:ring-primary  sm:text-sm sm:leading-6 duration-200`;
+export const Select = tw.select`block w-full outline-none rounded-md border-0 py-1.5 pl-3 pr-10 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-1 focus:ring-primary  sm:text-sm sm:leading-6 duration-200`;
 const FormControl = tw.div`flex flex-col gap-2`;
 
-export const SelectFieldControl = ({ control, name, label, options, disabled, placeholder, rules }) => {
+/**
+ *
+ * @property {string} name
+ * @returns
+ */
+export const SelectFieldControl = ({ initialValue, control, name, label, options, disabled, placeholder, rules }) => {
 	const id = useId();
 	return (
 		<Controller
@@ -25,7 +30,7 @@ export const SelectFieldControl = ({ control, name, label, options, disabled, pl
 							disabled={disabled}
 							defaultValue={value}
 							placeholder={placeholder}>
-							<option>Select</option>
+							<option>{initialValue || "Chọn"}</option>
 							{Array.isArray(options) &&
 								options.map((option, index) => (
 									<option value={option?.value} key={index}>
--- src/Core/components/common/TextFieldControl.jsx
+++ src/Core/components/common/TextFieldControl.jsx
@@ -1,7 +1,7 @@
 import { Controller } from "react-hook-form";
 import tw from "twin.macro";
 
-const Input = tw.input`block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6`;
+export const Input = tw.input`block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 outline-none shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6`;
 const FormControl = tw.div`flex flex-col gap-2`;
 
 const TextFieldControl = (props) => {
