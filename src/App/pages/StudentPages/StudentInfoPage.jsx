d049ef52451c91b3e2005fc11e733e56485cd660
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-05-09 09:23:38 +0700

    get student data and render data

--- src/App/pages/StudentPages/StudentInfoPage.jsx
+++ src/App/pages/StudentPages/StudentInfoPage.jsx
@@ -1,43 +1,36 @@
 import { useGetStudentQuery } from "@/App/providers/apis/studentsApi";
 import { useSelector } from "react-redux";
-import axiosClient from "@/Core/configs/axiosConfig";
-import { useEffect } from "react";
+import { statusStudents } from "@/Core/constants/statusStudents";
 const StudentInfoPage = () => {
 	const user = useSelector((state) => state.auth?.user);
-	// const { data } = useGetStudentQuery(user.id);
-	// console.log(data);
-	// console.log(data);
-	// const StatusRegister={
-	// 	1:"Chưa đăng ký,"
-	// 	2:
-	// 	3:
-	// }
-	useEffect(() => {
-		const getStudent = async () => {
-			try {
-				const data = await axiosClient.get("/student/" + user.id);
-				console.log(data);
-			} catch (error) {
-				console.log(error);
-			}
-		};
-		getStudent();
-	}, []);
+	const { data,isLoading } = useGetStudentQuery(user.id);
+	console.log(data)
 	return (
 		<>
 			<section className="mb-8 grid grid-cols-2 ">
 				<div className="border-r-2">
 					<ul className="flex flex-col gap-3">
 						<h1 className="mb-3 text-lg font-bold">Thông tin đăng ký</h1>
-						<li>Họ và tên : Đỗ Xuân Hoàng</li>
-						<li>Ngành : Thiết kế trang web</li>
-						<li>Khóa học : 17.2</li>
-						<li>Email : hoang</li>
-						<li>Lựa chọn:</li>
-						<li>Công ty đã chọn</li>
-						<li>
-							Trạng thái sinh viên: <b>Chưa đăng ký </b>{" "}
-						</li>
+						<li>Họ và tên : {data?.name}</li>
+						<li>Ngành : {data?.majors?.name}</li>
+						<li>Khóa học :{data?.course}</li>
+						<li>Email : {data?.email}</li>
+						<li>                Lựa chọn :{' '}
+                {data?.support === 0
+                  ? 'Tự tìm nơi thực tập'
+                  : '' || data?.support === 1
+                  ? 'Nhận hỗ trợ từ nhà trường'
+                  : ''}</li>
+						<li>Công ty đã chọn:   {data.support === 1
+                  ? data?.business?.name
+                  : data?.nameCompany}{' '}</li>
+						{/* <li>
+							Trạng thái sinh viên:  <span style={{color: "#F99011"}}>{statusStudents.map((index) => {
+                  if (index.value === statusForm) {
+                    return index.title;
+                  }
+                })}{' '}</span>
+						</li> */}
 					</ul>
 				</div>
 				<div className="ml-5 flex flex-col justify-center">
---
+++ src/Core/constants/statusStudents.js
@@ -0,0 +1,55 @@
+
+export const statusStudents = [
+	{
+		title: 'Chờ kiểm tra CV',
+		value: 0,
+	},
+	{
+		title: 'Sửa lại CV',
+		value: 1,
+	},
+	{
+		title: 'Nhận CV',
+		value: 2,
+	},
+	{
+		title: 'Không đủ điều kiện',
+		value: 3,
+	},
+	{
+		title: 'Đã nộp biên bản',
+		value: 4,
+	},
+	{
+		title: 'Sửa biên bản',
+		value: 5,
+	},
+	{
+		title: 'Đang thực tập',
+		value: 6,
+	},
+	{
+		title: 'Đã nộp báo cáo',
+		value: 7,
+	},
+	{
+		title: 'Sửa báo cáo',
+		value: 8,
+	},
+	{
+		title: 'Hoàn thành',
+		value: 9,
+	},
+	{
+		title: 'Chưa đăng ký',
+		value: 10,
+	},
+	{
+		title: 'Sửa biên bản',
+		value: 52,
+	},
+	{
+		title: 'Không đạt',
+		value: 12,
+	},
+];
