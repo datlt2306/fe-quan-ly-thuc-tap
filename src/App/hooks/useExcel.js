c50ea85b44c09caed45d05690b4bbaafe07ca3c0
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-12 04:19:34 +0700

    update useExcel hook

--- src/App/hooks/useExcel.js
+++ src/App/hooks/useExcel.js
@@ -1,66 +1,65 @@
 import { AllowedFileExt } from "@/Core/constants/allowedFileType";
-import mapExcelData from "@/Core/utils/mapExcelData";
+import { mapExcelData } from "@/Core/utils/excelDataHandler";
 import { useState } from "react";
+
 import * as XLSX from "xlsx";
 
 /**
- * @returns Import file handler and data from excel file
+ * @returns {[(file: File, callback: (data) => void) => void, {isLoading: boolean, isError: boolean}]}
  */
 export const useImportFromExcel = () => {
-	const [data, setData] = useState([]);
-	const [error, setError] = useState(null);
-	const handleGetFile = (file) => {
-		/* Boilerplate to set up FileReader */
-		const reader = new FileReader();
-		const rABS = !!reader.readAsBinaryString;
-		reader.onload = (e) => {
-			try {
-				/* chuyển đổi data*/
-				const bstr = e?.target && e.target.result;
-				const workbook = XLSX.read(bstr, { type: rABS ? "binary" : "array" });
-				/* lấy all sheet */
-				let sheets = [];
-				workbook.SheetNames.forEach((item) => {
-					const worksheet = workbook.Sheets[item];
-					/* Convert array of arrays */
-					const data = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
-					sheets = [...sheets, ...data.filter((row) => row.length > 0)];
-				});
+	const [importState, setImportState] = useState({
+		isLoading: false,
+		isError: false,
+	});
 
-				setData(sheets);
-				setError(null);
-			} catch (error) {
-				console.log(error);
-				setData([]);
-				setError({ message: error.message, success: false });
-			}
-		};
-		if (rABS) reader.readAsBinaryString(file);
-		else reader.readAsArrayBuffer(file);
+	/**
+	 *
+	 * @param {File} file
+	 * @param {(data: Array<{[key:string]: any}>) => void} callback
+	 */
+	const handleImportFile = (file, callback) => {
+		try {
+			setImportState({ isLoading: true, isError: false });
+			const reader = new FileReader();
+			reader.onload = (e) => {
+				/* Mapping data */
+				const workbook = XLSX.read(e.target.result, { type: "binary" });
+				const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+				const importedData = XLSX.utils.sheet_to_json(worksheet, { header: 1 });
+				const filteredData = importedData.filter((row) => !!row.length);
+
+				const mappedSheetData = mapExcelData(filteredData);
+				/* Excute callback after importing file */
+				if (callback instanceof Function) {
+					callback(mappedSheetData);
+				}
+			};
+
+			reader.readAsBinaryString(file);
+			setImportState({ isLoading: false, isError: false });
+		} catch (error) {
+			setImportState({ isLoading: false, isError: true });
+		}
 	};
 
-	return { handleGetFile, data, error };
+	return [handleImportFile, importState];
 };
 
-/**
- * @param {string} fileName
- * @param {Array} data
- * @returns Excel file
- */
 export const useExportToExcel = () => {
 	const [error, setError] = useState(null);
-	const handleExport = ({ fileName, data }) => {
+
+	const handleExportFile = ({ fileName, data }) => {
 		try {
-			/* convert state to workbook */
-			data = data.map((row) => row.map((cellValue) => cellValue.toString()));
 			const worksheet = XLSX.utils.aoa_to_sheet(data);
 			const workbook = XLSX.utils.book_new();
 			XLSX.utils.book_append_sheet(workbook, worksheet, "SheetJS");
-			/* generate XLSX file and send to client */
+			/* Generate XLSX file and send to client */
 			return XLSX.writeFile(workbook, fileName + AllowedFileExt.XLSX);
 		} catch (error) {
+			console.log(error);
 			setError(error.message);
 		}
 	};
-	return { handleExport, error };
+	return { handleExportFile, error };
 };
