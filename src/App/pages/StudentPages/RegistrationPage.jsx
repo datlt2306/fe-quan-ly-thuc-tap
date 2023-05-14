24a3ef6c950db7c5077910056cfabc15988ed193
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-05-14 10:42:45 +0700

    QLTT-10 render form layout  and create schema form

--- src/App/pages/StudentPages/RegistrationPage.jsx
+++ src/App/pages/StudentPages/RegistrationPage.jsx
@@ -3,14 +3,26 @@ import tw from "twin.macro";
 import InputFieldControl from "@/Core/components/common/FormControl/InputFieldControl";
 import SelectFieldControl from "@/Core/components/common/FormControl/SelectFieldControl";
 import Button from "@/Core/components/common/Button";
-import { useMemo, useState, useRef,useEffect } from "react";
+import { useMemo, useState, useRef, useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import {
 	formSignUpSchoolSupportSchema,
 	formSignUpSelfFindingSchema,
 } from "@/App/schemas/formSignUpInterShipSchema";
+
 const Layout = tw.div`grid grid-cols-12 gap-7 sm:gap-4 items-center my-4 `;
+const RegistrationTypeCol = tw.div`col-span-4  flex justify-end font-medium sm:col-span-12 sm:justify-start`;
+const FormSignUpCol = tw.div`col-span-8 flex items-center gap-4   sm:(col-span-12 items-start justify-start flex-col)`;
+
+const RegistrationType = {
+	SelfFinding: 0,
+	SchoolSupport: 1,
+};
+
+const LabelLayout = tw.label`inline-flex items-center`;
+const TitleForm = tw.span`ml-2 font-medium`;
+
 const FormRow = ({ label, children, note = false }) => (
 	<Layout>
 		<div className="col-span-4 flex justify-end font-medium sm:justify-start">
@@ -28,123 +40,244 @@ const FormRow = ({ label, children, note = false }) => (
 
 const RegistrationPage = () => {
 	const [file, setFile] = useState("");
-	useEffect(() => {
-	  
-	console.log(file);
-	  
-	}, [file])
-	
-	const { control, handleSubmit, register } = useForm({
-		resolver: yupResolver(formSignUpSchoolSupportSchema),
+	const [selectedOption, setSelectedOption] = useState(null);
+
+	const { control, handleSubmit, reset } = useForm({
+		resolver:
+			selectedOption == RegistrationType.SchoolSupport
+				? yupResolver(formSignUpSchoolSupportSchema)
+				: yupResolver(formSignUpSelfFindingSchema),
+		defaultValues: {
+			phone: "",
+			address: "",
+		},
 	});
 
-	const RegistrationType = {
-		SelfFinding: 0,
-		SchoolSupport: 1,
-	};
 	const onSubmit = async (data) => {
 		console.log(data);
 	};
-	const formRows = useMemo(
-		() => [
-			{
-				label: "Mã sinh viên",
-				content: <p>PH24952</p>,
-			},
-			{
-				label: "Họ và tên",
-				content: <p>MInh nguyễn</p>,
-			},
-			{
-				label: "Số điện thoại",
-				note: true,
-				content: (
-					<InputFieldControl control={control} name="phone" placeholder="Số điện thoại" />
-				),
-			},
-			{
-				label: "Địa chỉ",
-				note: true,
-				content: <InputFieldControl control={control} name="address" placeholder="Địa chỉ" />,
-			},
-			{
-				label: "Chuyên ngành",
-				note: true,
-				content: (
-					<InputFieldControl control={control} name="narrow" placeholder="Chuyên ngành" />
-				),
-			},
-			{
-				label: "Đơn vị thực tập",
-				note: true,
-				content: (
-					<InputFieldControl
-						control={control}
-						name="business"
-						placeholder="Chọn doanh nghiệp"
-					/>
-				),
-			},
-			{
-				label: "Vị trí thực tập",
-				note: true,
-				content: (
-					<InputFieldControl
-						control={control}
-						name="dream"
-						placeholder="VD: Web Back-end, Dựng phim, Thiết kế nội thất"
-					/>
-				),
-			},
-			{
-				label: "Upload CV(PDF)",
-				note: true,
-				content: (
-					<InputFieldControl
-						type="file"
-						control={control}
-						name="upload"
-						value={file}
-						onChange={(e) => setFile(e.target.value)}
-					/>
-				),
-			},
-			{
-				content: (
-					<Button type="submit" className="mt-5 bg-secondary text-white">
-						Đăng ký
-					</Button>
-				),
-			},
-		],
-		[control, file]
-	);
+	const labelItem = [
+		{
+			label: "Nhờ nhà trường hỗ trợ",
+			value: RegistrationType.SchoolSupport,
+		},
+		{
+			label: "Tự tìm nơi thực tập",
+			value: RegistrationType.SelfFinding,
+		},
+	];
+	const sharedFields=[
+		{
+			label: "Mã sinh viên",
+			content: <p>PH24952</p>,
+		},
+		{
+			label: "Họ và tên",
+			content: <p>MInh nguyễn</p>,
+		},
+		{
+			label: "Số điện thoại",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="phone"
+					placeholder="Số điện thoại"
+				/>
+			),
+		},
+		{
+			label: "Địa chỉ",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="address"
+					placeholder="Địa chỉ"
+				/>
+			),
+		},
+		{
+			label: "Chuyên ngành",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="narrow"
+					placeholder="Chuyên ngành"
+				/>
+			),
+		},
+		{
+			label: "Đơn vị thực tập",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="business"
+					placeholder="Chọn doanh nghiệp"
+				/>
+			),
+		},
+		{
+			label: "Vị trí thực tập",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="dream"
+					placeholder="VD: Web Back-end, Dựng phim, Thiết kế nội thất"
+				/>
+			),
+		}
+	]
+	const formSchoolSupport = [
+		...sharedFields,
+		{
+			label: "Upload CV(PDF)",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					type="file"
+					control={control}
+					name="upload"
+					value={file}
+					onChange={(e) => setFile(e.target.value)}
+				/>
+			),
+		},
+		{
+			content: (
+				<Button type="submit" className="mt-5 bg-secondary text-white">
+					Đăng ký
+				</Button>
+			),
+		},
+	];
+	const formSelfFinding = [
+			...sharedFields,
+		{
+			label: "Đơn vị thực tập",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="business"
+					placeholder="Chọn doanh nghiệp"
+				/>
+			),
+		},
+		{
+			label: "Địa chỉ thực tập",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="business"
+					placeholder="Chọn doanh nghiệp"
+				/>
+			),
+		},
+		{
+			label: "Mã số thuế",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="taxCode"
+					placeholder="Mã số thuế"
+				/>
+			),
+		},
+		{
+			label: "Chức vụ người tiếp nhận",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="position"
+					placeholder="Email người tiếp nhận"
+				/>
+			),
+		},
+		{
+			label: "Số điện thoại doanh nghiệp",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="numberEnterprise"
+					placeholder="Số điện thoại doanh nghiệp"
+				/>
+			),
+		},
+		{
+			label: "Email người tiếp nhận",
+			note: true,
+			content: (
+				<InputFieldControl
+					className="w-96 sm:w-full"
+					control={control}
+					name="emailEnterprise"
+					placeholder="Email người tiếp nhận"
+				/>
+			),
+		},
+
+		{
+			content: (
+				<Button type="submit" className="mt-5 bg-secondary text-white">
+					Đăng ký
+				</Button>
+			),
+		},
+	];
+
+	const handleChangeForm = (value) => {
+		setSelectedOption(value);
+		reset();
+	};
+
+	//kiểm tra option  hiện tại có phải null không null thì không render ra cái gì nếu không
+	//thì render theo option
+	const formToRender =
+		selectedOption != null
+			? selectedOption === RegistrationType.SchoolSupport
+				? formSchoolSupport
+				: formSelfFinding
+			: null;
 	return (
 		<>
 			<div className="mb-5">Thời gian đăng ký còn lại</div>
-
 			<form onSubmit={handleSubmit(onSubmit)}>
 				<Layout>
-					<div className="col-span-4  flex justify-end font-medium sm:col-span-12 sm:justify-start">
-						Kiểu đăng ký
-					</div>
-					<div tw="col-span-8 flex items-center gap-4   sm:(col-span-12 items-start justify-start flex-col) ">
-						{/* col-span-8 sm:col-span-12 sm:justify-start items-center gap-4  */}
-						<label className="inline-flex items-center">
-							<input
-								type="radio"
-								name="radio"
-								defaultValue={RegistrationType.SchoolSupport}
-							/>
-							<span className="ml-2 font-medium">Nhờ nhà trường hỗ trợ</span>
-						</label>
-						<label className="inline-flex items-center">
-							<input type="radio" name="radio" defaultValue={RegistrationType.SelfFinding} />
-							<span className="ml-2 font-medium">Tự tìm nơi thực tập</span>
-						</label>
-					</div>
+					<RegistrationTypeCol>Kiểu đăng ký</RegistrationTypeCol>
+					<FormSignUpCol>
+						{labelItem.map((item, index) => (
+							<LabelLayout key={index}>
+								<input
+									type="radio"
+									name="radio"
+									defaultValue={item.value}
+									onClick={() => handleChangeForm(item.value)}
+								/>
+								<TitleForm>{item.label}</TitleForm>
+							</LabelLayout>
+						))}
+					</FormSignUpCol>
 				</Layout>
-				{formRows.map((row, index) => (
+				{formToRender?.map((row, index) => (
 					<FormRow key={index} label={row.label} note={row.note}>
 						{row.content}
 					</FormRow>
--- src/App/schemas/formSignUpInterShipSchema.js
+++ src/App/schemas/formSignUpInterShipSchema.js
@@ -7,9 +7,7 @@ export const formSignUpSchoolSupportSchema = yup.object().shape({
 	narrow: yup.string().required('Vui lòng chọn chuyên ngành'),
 	phone: yup.string().matches(/(84|0[3|5|7|8|9])+([0-9]{8})\b/,"Số điện thoại không hợp lệ"
    ).required("Vui lòng nhập  số điện thoại"),
-   upload: yup.mixed().required('File không được để trống').test('fileFormat', 'Chỉ hỗ trợ định dạng PNG', (value) => {
-      return value && value.includes("png");
-    }),
+   c: yup.mixed().required('File không được để trống'),
 });   
 export const formSignUpSelfFindingSchema = yup.object().shape({
 	address: yup.string().required("Địa chỉ không được để trống"),
@@ -17,4 +15,11 @@ export const formSignUpSelfFindingSchema = yup.object().shape({
 	dream: yup.string().required("Vui lòng nhập vị trí thực tập"),
 	narrow: yup.string().required('Vui lòng chọn chuyên ngành'),
 	phone: yup.string().required("Vui lòng nhập đúng số điện thoại"),
+	taxCode: yup.string().matches(/^[0-9]*$/,"Mã số thuế không hợp lệ"
+   ).required("Vui lòng nhập Mã số thuế"),
+	position: yup.string().required("Vui lòng nhập chức vụ người tiếp nhận"),
+	numberEnterprise: yup.string().min(10,"Phải có ít nhất 10 kí tự").max(13,"Tối đa 13 kí tự").matches(/^(0|84)(2(0[3-9]|1[0-6|8|9]|2[0-2|5-9]|3[2-9]|4[0-9]|5[1|2|4-9]|6[0-3|9]|7[0-7]|8[0-9]|9[0-4|6|7|9])|3[2-9]|5[5|6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])([0-9]{7})$/,"Số điện thoại doanh nghiệp không hợp lệ").required("Vui lòng nhập Số điện thoại doanh nghiệp"),
+	emailEnterprise: yup.string().email("Email không hợp lệ").required("Vui lòng nhập email người tiếp nhận"),
+
+	
 });
