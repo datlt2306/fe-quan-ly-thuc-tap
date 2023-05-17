efe12596b1c78a9369c9558053d0d0078fd914ba
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-17 11:21:00 +0700

    Fix submenu icon size

--- src/App/layouts/MainLayout/components/DesktopSidebar.jsx
+++ src/App/layouts/MainLayout/components/DesktopSidebar.jsx
@@ -24,30 +24,24 @@ const DesktopSidebar = ({ navigation }) => {
 					<Menu>
 						{navigation.map((item) =>
 							!!item.children ? (
-								<Menu.Item key={item.label} className="rounded-md">
+								<Menu.Item key={item.name} className="rounded-md">
 									<Disclosure as="div">
 										{({ open }) => {
 											return (
 												<>
 													<Disclosure.Button
-														className={classNames(
-															"z-10 flex w-full items-center justify-between p-2 text-gray-800 hover:bg-gray-100",
-															{
-																"bg-gray-50": open,
-															}
-														)}>
+														className={classNames("z-10 flex w-full items-center justify-between p-2 text-gray-800 hover:bg-gray-100", {
+															"bg-gray-50": open,
+														})}>
 														<span className="flex flex-1 items-center gap-2 text-gray-500">
 															<item.icon className="h-6 w-6" />
-															{item.label}
+															{item.name}
 														</span>
 
 														<ChevronDownIcon
-															className={classNames(
-																"h-4 w-4 transform text-gray-500 duration-300 ease-in-out",
-																{
-																	"tranform rotate-180": open,
-																}
-															)}
+															className={classNames("h-4 w-4 transform text-gray-500 duration-300 ease-in-out", {
+																"tranform rotate-180": open,
+															})}
 														/>
 													</Disclosure.Button>
 													<Transition
@@ -55,26 +49,20 @@ const DesktopSidebar = ({ navigation }) => {
 														enterFrom="opacity-0 -translate-y-4 max=h-0"
 														enterTo="opacity-100 translate-y-0 max-h-none"
 														leave="transition duration-200 transform"
-														leaveFrom="opacity-50 translate-y-0 max-h-none"
+														leaveFrom="translate-y-0 max-h-none"
 														leaveTo="opacity-0 -translate-y-4 max-h-0">
-														<Disclosure.Panel
-															className="z-0 bg-gray-50 "
-															as={Menu.Items}>
+														<Disclosure.Panel className="z-0 bg-gray-50 " as={Menu.Items}>
 															{item.children.map((child, index) => {
 																return (
 																	child?.show === true && (
 																		<Menu.Item
 																			key={index}
 																			className="py-2 pl-10 pr-2 transition-[height_350ms_ease-in-out_transform] duration-300 hover:bg-gray-100 focus:active:bg-gray-100">
-																			<NavLink
-																				to={child.path}
-																				className={({ isActive }) =>
-																					navlinkClasses(isActive)
-																				}>
-																				<child.icon className="h-3 w-3 text-gray-500" />{" "}
-																				{child.label}
+																			<NavLink to={child.path} className={({ isActive }) => navlinkClasses(isActive)}>
+																				<child.icon className="h-5 w-5 " /> {child.name}
 																			</NavLink>
-																		</Menu.Item>)
+																		</Menu.Item>
+																	)
 																);
 															})}
 														</Disclosure.Panel>
@@ -85,15 +73,10 @@ const DesktopSidebar = ({ navigation }) => {
 									</Disclosure>
 								</Menu.Item>
 							) : (
-								<Menu.Item key={item.label} className="rounded-md">
-									<NavLink
-										to={item.path}
-										className={({ isActive }) => navlinkClasses(isActive)}>
-										<item.icon
-											className="h-6 w-6 shrink-0 text-[inherit]"
-											aria-hidden="true"
-										/>
-										{item.label}
+								<Menu.Item key={item.name} className="rounded-md">
+									<NavLink to={item.path} className={({ isActive }) => navlinkClasses(isActive)}>
+										<item.icon className="h-6 w-6 shrink-0 text-[inherit]" aria-hidden="true" />
+										{item.name}
 									</NavLink>
 								</Menu.Item>
 							)
