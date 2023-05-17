20a4e9250a935db32927955515d67bea43f763ac
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-17 16:36:17 +0700

    (QLTT-77) Fix: missing available roles in create manager form

--- src/App/schemas/staffSchema.js
+++ src/App/schemas/staffSchema.js
@@ -1,8 +1,11 @@
+import { RoleStaffEnum } from "@/Core/constants/roleStaff";
 import { object, string, array, number } from "yup";
 
-export const staffDataValidator = 
-	object({
-		name: string().required("Tên nhân viên là bắt buộc"),
-		email: string().email("Email không đúng định dạng").required("Email là bắt buộc"),
-		role: number().required("Quyền hạn là bắt buộc"),
-	});
+export const staffDataValidator = object({
+	name: string().required("Tên nhân viên là bắt buộc"),
+	email: string()
+		.email("Email không hợp lệ!")
+		.matches(/^[\w-\.]+@fpt\.edu\.vn$/, { message: "Email nhân viên phải là mail FPT!" })
+		.required("Vui lòng nhập đầy đủ email của nhân viên"),
+	role: number().required("Quyền hạn là bắt buộc"),
+});
