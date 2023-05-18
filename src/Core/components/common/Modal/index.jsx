c93ddfef3da1184d3b28bd7d5a1f92b85443b5c4
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-18 18:01:51 +0700

    Reverse deleted modal component

--- src/Core/components/common/Modal/index.jsx
+++ src/Core/components/common/Modal/index.jsx
@@ -29,9 +29,7 @@ export default function Modal({ openState, onOpenStateChange, title, children })
 							leaveTo="opacity-0 scale-95">
 							<Dialog.Panel className="relative w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
 								{!!title && (
-									<Dialog.Title
-										as="h3"
-										className="mb-6 text-lg font-medium leading-6 text-gray-900">
+									<Dialog.Title as="h3" className="mb-6 text-lg font-medium leading-6 text-gray-900">
 										{title}
 									</Dialog.Title>
 								)}
