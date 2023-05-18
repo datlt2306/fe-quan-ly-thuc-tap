a139d565f3f88fa41c8ad519410736068e8c0c57
IceTea <iceteawebdev@gmail.com>
2023-05-18 15:56:36 +0700

    (QLTT-11)

--- src/App/pages/StudentPages/RecordPage/RecordPage.jsx
+++ src/App/pages/StudentPages/RecordPage/RecordPage.jsx
@@ -2,7 +2,7 @@ import { recordSchema } from '@/App/schemas/recordSchema'
 import Button from '@/Core/components/common/Button'
 import InputFieldControl from '@/Core/components/common/FormControl/InputFieldControl'
 import { yupResolver } from '@hookform/resolvers/yup'
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import { useForm } from 'react-hook-form'
 import { useSelector } from 'react-redux'
 import tw from 'twin.macro'
@@ -11,19 +11,36 @@ const RecordPage = () => {
   const { user } = useSelector((state) => state.auth)
 
   const [selectFile, setSelectFile] = useState(null);
+  const [validateFile, setValidateFile] = useState("");
+
+  const fileInputRef = useRef(null);
 
   const { handleSubmit, control } = useForm({
     resolver: yupResolver(recordSchema)
   })
 
   const handleChange = (e) => {
-    setSelectFile(e.target.files[0])
-  }
+    const file = e.target.files[0];
+    const allowedExtensions = ['jpg', 'jpeg', 'png', 'gif', 'pdf', 'docx'];
+    if (!file) {
+      setValidateFile('Không có file được chọn');
+      return;
+    }
+    // Get the file extension
+    const fileName = file.name;
+    const fileExtension = fileName.split('.').pop().toLowerCase();
 
-  const onSubmit = (value) => {
-    console.log(selectFile)
-    console.log(value)
+    if (!allowedExtensions.includes(fileExtension)) {
+      setValidateFile('File không đúng định dạng');
+      return;
+    } else {
+      setValidateFile('');
+      setSelectFile(e.target.files[0])
+    }
   }
+  const onSubmit = (value) => {
+    console.log('Form values:', value);
+  };
   return (
     <Container>
       <Title>Nộp biên bản</Title>
@@ -32,7 +49,8 @@ const RecordPage = () => {
         <Info>Họ và tên: <Span>{user && user?.displayName}</Span></Info>
         <InputFieldControl label="Tên doanh nghiệp" placeholder="Tên doanh nghiệp ..." control={control} name="companyName" />
         <InputFieldControl label="Thời gian bắt đầu thực tập" control={control} name="date" type="date" />
-        <InputFieldControl label="Upload biên bản (Image, PDF hoặc Docx)" control={control} name="form" type="file" onChange={handleChange} />
+        <InputFieldControl ref={fileInputRef} label="Upload (Image, PDF hoặc Docx)" control={control} name="form" type="file" onChange={handleChange} />
+        <Error>{validateFile}</Error>
         <Button variant="primary" type="submit">Nộp biên bản</Button>
       </Form>
     </Container>
@@ -43,6 +61,7 @@ const Form = tw.form`grid gap-8`
 const Title = tw.div`mb-8 text-primary text-xl font-bold`;
 const Container = tw.div`container mx-auto w-[512px]`
 const Info = tw.div``
+const Error = tw.div`text-error`
 const Span = tw.span`font-bold`
 
 export default RecordPage
