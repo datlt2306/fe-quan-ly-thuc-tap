97aa880d615d98a846ed0417779642d8011b398f
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-19 16:14:19 +0700

    Fix: duplicate validate students list

--- src/App/schemas/studentSchema.js
+++ src/App/schemas/studentSchema.js
@@ -1,22 +1,31 @@
-import { StudentSchoolingStatus } from "@/Core/constants/studentStatus";
-import { object, string, array } from "yup";
+import * as yup from "yup";
 
-export const newStudentSchema = array(
-	object({
-		mssv: string().required("Vui lòng nhập đầy đủ mã sinh viên"),
-		name: string().required("Vui lòng nhập đầy đủ tên sinh viên"),
-		course: string().required("Vui lòng nhập đầy đủ khóa nhập học của sinh viên"),
-		majorCode: string().required("Vui lòng nhập đầy đủ mã ngành của sinh viên"),
-		email: string()
-			.email()
-			.matches(/^[\w-\.]+@fpt\.edu\.vn$/, { message: "Email sinh viên không đúng định dạng" })
-			.required("Vui lòng nhập đầy đủ email của sinh viên"),
-		phoneNumber: string()
-			.matches(/^\d{10}$/, { message: "Số điện thoại của sinh viên chưa đúng định dạng" })
-			.required("Vui lòng nhập đầy đủ số điện thoại của sinh viên"),
-		statusStudent: string().required("Vui lòng nhập đầy đủ trạng thái sinh viên"),
-		smester_id: string().required("Vui lòng nhập đầy đủ kỳ học hiện tại của sinh viên"),
-		note: string().optional(),
-	})
-);
+yup.addMethod(yup.array, "unique", function (message, mapper = (a) => a) {
+	return this.test("unique", message, function (list) {
+		return list.length === new Set(list.map(mapper)).size;
+	});
+});
 
+export const newStudentSchema = yup
+	.array(
+		yup.object({
+			mssv: yup.string().required("Vui lòng nhập đầy đủ mã sinh viên"),
+			name: yup.string().required("Vui lòng nhập đầy đủ tên sinh viên"),
+			course: yup.string().required("Vui lòng nhập đầy đủ khóa nhập học của sinh viên"),
+			majorCode: yup.string().required("Vui lòng nhập đầy đủ mã ngành của sinh viên"),
+			email: yup
+				.string()
+				.email()
+				.matches(/^[\w-\.]+@fpt\.edu\.vn$/, { message: "Email sinh viên không đúng định dạng" })
+				.required("Vui lòng nhập đầy đủ email của sinh viên"),
+			phoneNumber: yup
+				.string()
+				.matches(/^\d{10}$/, { message: "Số điện thoại của sinh viên chưa đúng định dạng" })
+				.required("Vui lòng nhập đầy đủ số điện thoại của sinh viên"),
+			statusStudent: yup.string().required("Vui lòng nhập đầy đủ trạng thái sinh viên"),
+			smester_id: yup.string().required("Vui lòng nhập đầy đủ kỳ học hiện tại của sinh viên"),
+			note: yup.string().optional(),
+		})
+	)
+	.unique("Email không được trùng !", (value) => value.email)
+	.unique("Mã sinh viên không được trùng !", (value) => value.mssv);
