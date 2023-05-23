63e52b8a7d9266e34277e86674af55ddd7798725
TuanAnhNguyen <tuananh1210085213@gmail.com>
2023-05-23 16:51:28 +0700

    (QLTT-97) manager campus

--- src/App/providers/apis/campusApi.js
+++ src/App/providers/apis/campusApi.js
@@ -9,10 +9,28 @@ const campusApi = createApi({
 		getAllCampus: build.query({
 			query: (params) => ({ url: '/campus', method: 'GET', params }),
 			providesTags: ['Campus']
+		}),
+		addCampus: build.mutation({
+			query: (payload) => {
+				return { url: '/campus', method: 'POST', data: payload };
+			},
+			invalidatesTags: ['Campus']
+		}),
+		updateCampus: build.mutation({
+			query: ({ id, payload }) => {
+				return { url: '/campus/' + id, method: 'PATCH', data: payload };
+			},
+			invalidatesTags: ['Campus']
+		}),
+		deleteCampus: build.mutation({
+			query: (id) => {
+				return { url: '/campus/' + id, method: 'DELETE' };
+			},
+			invalidatesTags: ['Campus']
 		})
 	})
 });
 
-export const { useGetAllCampusQuery } = campusApi;
+export const { useGetAllCampusQuery, useAddCampusMutation, useDeleteCampusMutation, useUpdateCampusMutation } = campusApi;
 
 export default campusApi;
