647b407b3127a93946877b8b4d720952bfa9031d
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-25 19:43:22 +0700

    Fix: import studentConstants from incorrect path

--- src/App/pages/StaffPages/ReviewsPage/ReviewCvPage.jsx
+++ src/App/pages/StaffPages/ReviewsPage/ReviewCvPage.jsx
@@ -1,21 +1,18 @@
 import {
 	InternSupportType,
-	StudentColumnAccessors,
 	StudentReviewTypeEnum,
 	StudentStatusEnum,
 	StudentStatusGroupEnum
 } from '@/App/constants/studentConstants';
 import { useExportToExcel } from '@/App/hooks/useExcel';
 import { useGetStudentsToReviewQuery } from '@/App/providers/apis/studentApi';
-import Badge from '@/Core/components/common/Badge';
 import Button from '@/Core/components/common/Button';
 import ReactTable from '@/Core/components/common/Table/ReactTable';
-import { InputColumnFilter, SelectColumnFilter } from '@/Core/components/common/Table/ReactTableFilters';
-import IndeterminateCheckbox from '@/Core/components/common/Table/RowSelectionCheckbox';
 import formatDate from '@/Core/utils/formatDate';
-import { ChatBubbleLeftEllipsisIcon, EyeIcon, EyeSlashIcon } from '@heroicons/react/24/outline';
+import { ChatBubbleLeftEllipsisIcon } from '@heroicons/react/24/outline';
 import { Fragment, useMemo, useState } from 'react';
 import tw from 'twin.macro';
+import InstanceStudentColumns from '../Shared/InstanceStudentColumns';
 import UpdateReviewModal from './components/UpdateReviewModal';
 
 const handleGetInternStatusStyle = (value) => {
@@ -66,147 +63,11 @@ const ReviewCvPage = () => {
 
 	// Define columns of table
 	const columnsData = useMemo(
-		() => [
-			{
-				Header: ({ getToggleAllPageRowsSelectedProps }) => (
-					<IndeterminateCheckbox {...getToggleAllPageRowsSelectedProps()} />
-				),
-				accessor: '_id',
-				Cell: ({ row }) => {
-					const isDisabled = !(!!row.original?.CV || !!row.original?.form || !!row.original?.report);
-					return <IndeterminateCheckbox disabled={isDisabled} {...row.getToggleRowSelectedProps()} />;
-				}
-			},
-			{
-				Header: StudentColumnAccessors.index,
-				accessor: 'index',
-				sortable: true
-			},
-			{
-				Header: StudentColumnAccessors.name,
-				accessor: 'name',
-				Filter: InputColumnFilter,
-				filterable: true,
-				sortable: true
-			},
-			{
-				Header: StudentColumnAccessors.mssv,
-				accessor: 'mssv', // object key
-				Filter: InputColumnFilter,
-				filterable: true,
-				sortable: true,
-				Cell: ({ value }) => <span className='font-semibold uppercase'>{value}</span>
-			},
-			{
-				Header: StudentColumnAccessors.majorCode,
-				accessor: 'majorCode',
-				Filter: InputColumnFilter,
-				filterable: true,
-				sortable: true
-			},
-			{
-				Header: StudentColumnAccessors.phoneNumber,
-				accessor: 'phoneNumber',
-				Filter: InputColumnFilter,
-				filterable: true
-			},
-			{
-				Header: StudentColumnAccessors.email,
-				accessor: 'email',
-				Filter: InputColumnFilter,
-				filterable: true
-			},
-			{
-				Header: StudentColumnAccessors.support,
-				accessor: 'support',
-				Filter: SelectColumnFilter,
-				filterable: true
-			},
-			{
-				Header: StudentColumnAccessors.statusCheck,
-				accessor: 'statusCheck',
-				Filter: ({ column: { filterValue, setFilter, preFilteredRows, id } }) => (
-					<SelectColumnFilter
-						column={{ filterValue, setFilter, preFilteredRows, id }}
-						customOptions={StudentStatusEnum}
-					/>
-				),
-				filterable: true,
-				Cell: ({ value }) => <Badge variant={handleGetInternStatusStyle(value)}>{value}</Badge>
-			},
-
-			{
-				Header: StudentColumnAccessors.nameCompany,
-				accessors: 'nameCompany',
-				Filter: InputColumnFilter,
-				filterable: true,
-				Aggregated: ({ value }) => value,
-				Cell: ({ row }) =>
-					row.original?.nameCompany && (
-						<ul tw='flex flex-col gap-2'>
-							<li>
-								<strong>{row.original?.nameCompany}</strong>
-							</li>
-							<li>
-								<i>Mã số thuế: </i>
-								{row.original?.taxCode}
-							</li>
-						</ul>
-					)
-			},
-			{
-				Header: StudentColumnAccessors.dream,
-				accessor: 'dream',
-				Filter: InputColumnFilter,
-				filterable: true
-			},
-			{
-				Header: StudentColumnAccessors.CV,
-				accessor: 'CV',
-				Filter: InputColumnFilter,
-				filterable: false,
-				sortable: false,
-				Cell: ({ value }) => (
-					<Button
-						as='a'
-						href={value}
-						target='_blank'
-						variant={value ? 'ghost' : 'disabled'}
-						shape='square'
-						size='sm'
-						disabled={!!value}>
-						{value ? <EyeIcon className='h-4 w-4' /> : <EyeSlashIcon className='h-4 w-4' />}
-					</Button>
-				)
-			},
-			{
-				Header: StudentColumnAccessors.reviewer,
-				accessor: 'reviewer',
-				Filter: InputColumnFilter,
-				filterable: true
-			},
-			{
-				Header: StudentColumnAccessors.statusStudent,
-				accessor: 'statusStudent',
-				Filter: SelectColumnFilter,
-				filterable: true,
-				Cell: ({ value }) => <span className='font-semibold'>{value}</span>
-			},
-
-			{
-				Header: StudentColumnAccessors.createdAt,
-				accessor: 'createdAt',
-				Filter: InputColumnFilter,
-				filterable: true,
-				Cell: ({ value }) => <span>{formatDate(value)}</span>
-			},
-			{
-				Header: StudentColumnAccessors.note,
-				accessor: 'note',
-				filterable: false,
-				sortable: false
-			}
-		],
+		() =>
+			InstanceStudentColumns.filter((column) => {
+				const excludeColumns = ['report', 'form'];
+				return !excludeColumns.includes(column?.accessor);
+			}),
 		[]
 	);
 
