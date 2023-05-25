48799e3e08e8188d3542231e81a448f90e407011
TuanAnhNguyen <tuananh1210085213@gmail.com>
2023-05-25 17:50:35 +0700

    (QLTT-109) fix/select-role-staff

--- src/App/schemas/staffSchema.js
+++ src/App/schemas/staffSchema.js
@@ -4,7 +4,7 @@ export const staffDataValidator = object({
 	name: string().trim().required('Tên nhân viên là bắt buộc'),
 	email: string()
 		.email('Email không hợp lệ!')
-		.matches(/^[A-Z0-9._%+-]+@fe\.edu\.vn$/i, { message: 'Email nhân viên phải là mail FPT!' })
+		.matches(/^[A-Z0-9._%+-]+@fe\.edu\.vn$/i, { message: 'Email nhân viên phải là mail FE!' })
 		.required('Vui lòng nhập đầy đủ email của nhân viên'),
 	role: string().required('Quyền hạn là bắt buộc')
 });
