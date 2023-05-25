943183f9b8ece9e9a2b8365ef7c78c464997f7de
quanghiep03198 <quanghiep03198@gmail.com>
2023-05-25 12:49:49 +0700

    Update: yup regex validate in all schemas

--- src/App/schemas/formSignUpInterShipSchema.js
+++ src/App/schemas/formSignUpInterShipSchema.js
@@ -7,7 +7,7 @@ const sharedFieldSchema = {
 	phoneNumber: yup
 		.string()
 		.required('Vui lòng nhập  số điện thoại')
-		.matches(/^(0[3|5|7|8|9])+([0-9]{8})\b/, 'Số điện thoại không hợp lệ')
+		.matches(/^\d{10,11}$/, 'Số điện thoại không hợp lệ')
 		.default('')
 		.trim()
 };
@@ -37,10 +37,7 @@ export const formSignUpSelfFindingSchema = yup.object().shape({
 		.string()
 		.min(10, 'Phải có ít nhất 10 kí tự')
 		.max(13, 'Tối đa 13 kí tự')
-		.matches(
-			/^(0|84)(2(0[3-9]|1[0-6|8|9]|2[0-2|5-9]|3[2-9]|4[0-9]|5[1|2|4-9]|6[0-3|9]|7[0-7]|8[0-9]|9[0-4|6|7|9])|3[2-9]|5[5|6|8|9]|7[0|6-9]|8[0-6|8|9]|9[0-4|6-9])([0-9]{7})$/,
-			'Số điện thoại doanh nghiệp không hợp lệ'
-		)
+		.matches(/^\d{10,11}$/, 'Số điện thoại doanh nghiệp không hợp lệ')
 		.required('Vui lòng nhập số điện thoại doanh nghiệp')
 		.default('')
 		.trim(),
