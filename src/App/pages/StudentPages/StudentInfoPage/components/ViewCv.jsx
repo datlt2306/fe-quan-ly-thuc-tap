07cb6cdde6fb99e215c779b5f60f55b44053759c
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-05-27 18:26:59 +0700

    fix ui and fix get major

--- src/App/pages/StudentPages/StudentInfoPage/components/ViewCv.jsx
+++ src/App/pages/StudentPages/StudentInfoPage/components/ViewCv.jsx
@@ -1,6 +1,4 @@
 import Button from '@/Core/components/common/Button';
-import { useRequestOfStudentMutation } from '@/App/providers/apis/requestStudentsApi';
-import { toast } from 'react-toastify';
 import { VerticalList } from '..';
 
 import { InternSupportType } from '@/App/constants/studentConstants';
@@ -9,54 +7,54 @@ import tw from 'twin.macro';
 import FormRequestSupport from './FormRequestSupport';
 
 const ItemValue = tw.p`font-medium`;
-const ViewCv = ({ data, setOpenState, nameMajor }) => {
+const ViewCv = ({ data: user, setOpenState }) => {
 	const dataFormInterShip = [
-		{ label: 'Kiểu đăng ký :', value: InternSupportType[+data?.support] ?? null },
-		{ label: 'Mã sinh viên :', value: data?.mssv },
-		{ label: 'Họ tên :', value: data?.name },
-		{ label: 'Email:', value: data?.email },
-		{ label: 'Số điện thoại:', value: data?.phoneNumber },
-		{ label: 'Địa chỉ:', value: data?.address },
-		{ label: 'Chuyên ngành:', value: nameMajor },
-		{ label: 'Vị trí thực tập:', value: data?.dream },
+		{ label: 'Kiểu đăng ký :', value: InternSupportType[+user?.support] ?? null },
+		{ label: 'Mã sinh viên :', value: user?.mssv },
+		{ label: 'Họ tên :', value: user?.name },
+		{ label: 'Email:', value: user?.email },
+		{ label: 'Số điện thoại:', value: user?.phoneNumber },
+		{ label: 'Địa chỉ:', value: user?.address },
+		{ label: 'Chuyên ngành:', value: user?.major?.name },
+		{ label: 'Vị trí thực tập:', value: user?.dream },
 		{
 			label: (
 				<>
-					{data?.support === 1 ? (
+					{user?.support === 1 ? (
 						<>
 							<p className='pr-1 '>
-								Công ty: <span tw='font-medium'>:{data?.business?.name}</span>
+								Công ty: <span tw='font-medium'>:{user?.business?.name}</span>
 							</p>
 							<div className='mt-3 flex items-center'>
 								<p className='pr-1'>CV:</p>
-								<Button onClick={() => window.open(data?.['CV'])}>Xem</Button>
+								<Button onClick={() => window.open(user?.['CV'])}>Xem</Button>
 							</div>
 						</>
-					) : data?.support === 0 ? (
+					) : user?.support === 0 ? (
 						<div className='flex flex-col gap-3'>
 							<Flex>
 								<p>Tên công ty:</p>
-								<ItemValue>{data?.nameCompany}</ItemValue>
+								<ItemValue>{user?.nameCompany}</ItemValue>
 							</Flex>
 							<Flex>
 								<p>Địa chỉ thực tập:</p>
-								<ItemValue>{data?.addressCompany}</ItemValue>
+								<ItemValue>{user?.addressCompany}</ItemValue>
 							</Flex>
 							<Flex>
 								<p>Mã số thuế:</p>
-								<ItemValue>{data?.taxCode}</ItemValue>
+								<ItemValue>{user?.taxCode}</ItemValue>
 							</Flex>
 							<Flex>
 								<p>Chức vụ người tiếp nhận:</p>
-								<ItemValue>{data?.position}</ItemValue>
+								<ItemValue>{user?.position}</ItemValue>
 							</Flex>
 							<Flex>
 								<p>SĐT doanh nghiệp:</p>
-								<ItemValue>{data?.phoneNumberCompany}</ItemValue>
+								<ItemValue>{user?.phoneNumberCompany}</ItemValue>
 							</Flex>
 							<Flex>
 								<p>Email người tiếp nhận:</p>
-								<ItemValue>{data?.emailEnterprise}</ItemValue>
+								<ItemValue>{user?.emailEnterprise}</ItemValue>
 							</Flex>
 						</div>
 					) : null}
