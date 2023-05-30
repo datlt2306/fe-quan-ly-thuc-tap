8eeaa3c0344660ed44b980c462d1ece25f0166ac
IceTea <iceteawebdev@gmail.com>
2023-05-30 15:57:32 +0700

    (QLTT-122) request-student

--- src/App/pages/StaffPages/StudentSuportPage/index.jsx
+++ src/App/pages/StaffPages/StudentSuportPage/index.jsx
@@ -4,20 +4,23 @@ import {
 	useResetStudentRequestMutation
 } from '@/App/providers/apis/requestStudentsApi';
 import Button from '@/Core/components/common/Button';
+import { Option, Select } from '@/Core/components/common/FormControl/SelectFieldControl';
 import PopConfirm from '@/Core/components/common/Popup/PopConfirm';
 import ReactTable from '@/Core/components/common/Table/ReactTable';
 import { InputColumnFilter, SelectColumnFilter } from '@/Core/components/common/Table/ReactTableFilters';
+import { ListBulletIcon } from '@heroicons/react/24/outline';
 import moment from 'moment';
 import React, { useMemo, useState, useEffect } from 'react';
 import tw from 'twin.macro';
 
 const index = () => {
-	const { data } = useGetRequestOfStudentQuery();
+	const status = ['Đang xử lý', 'Đồng ý', 'Từ chối'];
+	const { data, refetch } = useGetRequestOfStudentQuery();
 	const [handleAccept] = useResetStudentRequestMutation();
 	const [handleReject] = useRemoveRequestApiMutation();
 	const [tableData, setTableData] = useState([]);
 	useEffect(() => {
-		setTableData(data?.data);
+		setTableData(data?.data.filter((item) => item.status === 1));
 	}, [data]);
 
 	const columnsData = useMemo(
@@ -50,7 +53,8 @@ const index = () => {
 			{
 				Header: 'Mô tả',
 				accessor: 'description',
-				Filter: InputColumnFilter
+				Filter: InputColumnFilter,
+				Cell: ({ value }) => <div className='max-w-xs whitespace-normal'>{value}</div>
 			},
 			{
 				Header: 'Ngày tạo',
@@ -64,19 +68,20 @@ const index = () => {
 				accessor: '_id',
 				Cell: ({ row }) => {
 					return (
-						<ActionList>
+						<ActionList className={row.original.status !== 1 ? 'hidden' : ''}>
 							<PopConfirm
 								okText='Ok'
 								cancelText='Cancel'
 								title={'Chấp nhận yêu cầu'}
 								description={'Bạn muốn chấp nhận yêu cầu này ?'}
-								onConfirm={() =>
-									handleAccept({
+								onConfirm={async () => {
+									await handleAccept({
 										userId: row.original.userId._id,
 										type: row.original.type,
 										id: row.original._id
-									})
-								}>
+									});
+									refetch();
+								}}>
 								<Button type='button' size='sm' variant='secondary'>
 									Đồng ý
 								</Button>
@@ -86,7 +91,10 @@ const index = () => {
 								cancelText='Cancel'
 								title={'Từ chối yêu cầu'}
 								description={'Bạn muốn từ chối yêu cầu này ?'}
-								onConfirm={() => handleReject(row.original._id)}>
+								onConfirm={async () => {
+									await handleReject(row.original._id);
+									refetch();
+								}}>
 								<Button type='button' size='sm' variant='error'>
 									Từ chối
 								</Button>
@@ -96,12 +104,34 @@ const index = () => {
 				}
 			}
 		],
-		[]
+		[data]
 	);
 
-	return <div>{tableData && <ReactTable columns={columnsData} data={tableData} />}</div>;
+	return (
+		<div>
+			<SelectBox>
+				<label
+					htmlFor='semester-list'
+					className='inline-flex items-center gap-2 whitespace-nowrap text-base-content'>
+					<ListBulletIcon className='h-6 w-6' /> Trạng thái<i></i>
+				</label>
+				<Select
+					className='max-w-[12rem] capitalize sm:text-sm'
+					onChange={(e) => setTableData(data?.data.filter((item) => item.status == e.target.value))}>
+					{Array.isArray(status) &&
+						status.map((item, index) => (
+							<Option value={index + 1} key={index} selected={index + 1 === 1}>
+								{item}
+							</Option>
+						))}
+				</Select>
+			</SelectBox>
+			{tableData && <ReactTable columns={columnsData} data={tableData} />}
+		</div>
+	);
 };
 
 const ActionList = tw.div`flex items-stretch gap-1`;
+const SelectBox = tw.div`flex basis-1/4 items-center gap-2 mb-4`;
 
 export default index;
