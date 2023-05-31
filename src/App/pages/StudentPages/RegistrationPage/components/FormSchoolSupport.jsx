c35f42259953dddcb528a4c0a0b73bf2fadd5dc2
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-05-31 21:10:54 +0700

    refactor code and auto full field when status fix cv

--- src/App/pages/StudentPages/RegistrationPage/components/FormSchoolSupport.jsx
+++ src/App/pages/StudentPages/RegistrationPage/components/FormSchoolSupport.jsx
@@ -1,49 +1,34 @@
-/* eslint-disable react/prop-types */
-
+import { useState, useEffect } from 'react';
 import { formSignUpSchoolSupportSchema } from '@/App/schemas/formSignUpInterShipSchema';
 import Button from '@/Core/components/common/Button';
 import FileUploadFieldControl from '@/Core/components/common/FormControl/FileUploadFieldControl';
-import SelectFieldControl from '@/Core/components/common/FormControl/SelectFieldControl';
+
 import { LoadingSpinner } from '@/Core/components/common/Loading/LoadingSpinner';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Fragment } from 'react';
 import { useForm } from 'react-hook-form';
 import { useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
 import tw from 'twin.macro';
 import FormRow from '../components/FormRow';
 import { useUploadCvMutation } from '@/App/providers/apis/internRegistrationApi';
-import SharedFields from './SharedFields';
+import SharedFields, { SharedDefaultValues } from './SharedFields';
+import ComboBoxFieldControl from '@/Core/components/common/FormControl/ComboBoxFieldControl';
+import { useGetAllCompanyQuery } from '@/App/providers/apis/businessApi';
 
-const FormSchoolSupport = ({ business, selectedOption, user, majors }) => {
+const FormSchoolSupport = ({ selectedOption, user }) => {
 	const navigate = useNavigate();
+	const { data: business, isLoading: loadingBusiness } = useGetAllCompanyQuery();
 	const [hanldeUploadCvMutation, { isLoading }] = useUploadCvMutation();
+
 	const { control, handleSubmit } = useForm({
 		resolver: yupResolver(formSignUpSchoolSupportSchema),
-		defaultValues: formSignUpSchoolSupportSchema.getDefault()
+		defaultValues: formSignUpSchoolSupportSchema.getDefault(),
+		values: user?.support == 1 &&
+			user?.statusCheck == 1 && {
+				...SharedDefaultValues({ user }),
+				business: user?.business?._id
+			}
 	});
-
-	const formSchoolSupport = [
-		...SharedFields(control, user, majors),
-		{
-			content: (
-				<SelectFieldControl
-					label='Đơn vị thực tập'
-					initialValue='Chọn doanh nghiệp'
-					control={control}
-					name='business'
-					options={
-						Array.isArray(business?.list)
-							? business.list.map((item) => ({ value: item._id, label: item.name }))
-							: []
-					}
-				/>
-			)
-		},
-		{
-			content: <FileUploadFieldControl label='Upload CV(PDF)' className='mt-1 w-96' control={control} name='CV' />
-		}
-	];
 
 	const handleFormSchoolSupport = async (data) => {
 		const file = data.CV;
@@ -56,10 +41,8 @@ const FormSchoolSupport = ({ business, selectedOption, user, majors }) => {
 		const handleData = {
 			...data,
 			typeNumber: selectedOption,
-			support: Number(selectedOption),
-			mssv: user?.mssv,
+			support: selectedOption,
 			_id: user?._id,
-			email: user?.email,
 			CV: '',
 			majorCode: user?.majorCode
 		};
@@ -68,7 +51,6 @@ const FormSchoolSupport = ({ business, selectedOption, user, majors }) => {
 			formData.append(key, value);
 		});
 		formData.append('file', file);
-		// const response = await uploadDriveMutation(formData);
 		const res = await hanldeUploadCvMutation(formData);
 		if (res?.error) {
 			toast.error('Đã có lỗi xảy ra');
@@ -81,9 +63,20 @@ const FormSchoolSupport = ({ business, selectedOption, user, majors }) => {
 		<>
 			<Form onSubmit={handleSubmit(handleFormSchoolSupport)}>
 				<FormRow>
-					{formSchoolSupport.map((row, index) => (
-						<Fragment key={index}>{row.content}</Fragment>
-					))}
+					<SharedFields control={control} student={user} />
+					<ComboBoxFieldControl
+						loading={loadingBusiness}
+						placeholder='Chọn doanh nghiệp'
+						control={control}
+						name='business'
+						label='Đơn vị thực tập'
+						options={
+							Array.isArray(business?.list)
+								? business.list.map((item) => ({ value: item._id, label: item.name }))
+								: []
+						}
+					/>
+					<FileUploadFieldControl label='Upload CV(PDF)' className='mt-1 w-96' control={control} name='CV' />
 				</FormRow>
 				<Button type='submit' variant='primary' className='mt-2' disabled={isLoading}>
 					{isLoading && <LoadingSpinner size='sm' variant='primary' />}
