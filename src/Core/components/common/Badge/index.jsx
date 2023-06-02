0e878033193921dc0ee4a4cb744bf1f12e9d2d8a
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-03 02:36:12 +0700

    Update core components

--- src/Core/components/common/Badge/index.jsx
+++ src/Core/components/common/Badge/index.jsx
@@ -1,16 +1,19 @@
 import classNames from 'classnames';
 import React from 'react';
 
-const Badge = ({ variant, ...props }) => {
+const Badge = ({ variant, size = 'sm', ...props }) => {
 	return (
 		<span
-			className={classNames('inline-flex items-center gap-x-1.5 rounded-sm px-1.5 py-1 text-xs font-semibold', {
+			className={classNames('inline-flex items-center gap-x-1.5 rounded-sm  font-semibold', {
 				'bg-primary/10 text-primary': variant === 'primary',
 				'bg-success/10 text-success': variant === 'success',
 				'bg-info/10 text-info': variant === 'info',
 				'bg-error/10 text-error': variant === 'error',
 				'bg-warning/10 text-warning': variant === 'warning',
-				'bg-disabled/10 text-disabled': variant === 'disabled'
+				'bg-disabled/10 text-disabled': variant === 'disabled',
+				'px-1.5 py-1 text-xs': size === 'sm',
+				'px-2 py-1.5 text-sm': size === 'md',
+				'px-2.5 py-1.5 text-base': size === 'lg'
 			})}>
 			{props.children}
 		</span>
