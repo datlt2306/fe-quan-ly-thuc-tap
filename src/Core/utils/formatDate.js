7832239cfca2b616e37fcbbc15f053f15aa49efa
nhatminh6112003 <nhatminhnguyen6112003@gmail.com>
2023-06-02 18:36:03 +0700

    fix conflict

--- src/Core/utils/formatDate.js
+++ src/Core/utils/formatDate.js
@@ -5,5 +5,5 @@ import moment from 'moment/moment';
  * @param {string | Date} date
  */
 export default function formatDate(date) {
-	return moment(date).format('D/M/YYYY');
+	return moment().format('DD/MM/YYYY', date);
 }
