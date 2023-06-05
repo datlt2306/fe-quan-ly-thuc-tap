10376e38b9a4f49e2ae61d1f94a2742533c4e34f
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-05 17:53:55 +0700

    Update form control core components

--- src/Core/components/common/FormControl/TextareaFieldControl.jsx
+++ src/Core/components/common/FormControl/TextareaFieldControl.jsx
@@ -63,6 +63,6 @@ const TextareaFieldControl = forwardRef(
 	}
 );
 
-const Textarea = tw.textarea`block w-full rounded-md border-0 duration-300 px-2.5 py-1.5 text-gray-900 outline-none shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6`;
+const Textarea = tw.textarea`block w-full rounded-md border-0 duration-300 px-2.5 py-1.5 text-base-content outline-none shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-1 focus:ring-inset focus:ring-primary sm:text-sm sm:leading-6`;
 
 export default TextareaFieldControl;
