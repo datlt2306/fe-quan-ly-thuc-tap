a01cfdcbfc5dfef185d5e9e5c47640624fe55d65
quanghiep03198 <quanghiep03198@gmail.com>
2023-06-08 12:24:37 +0700

    Update global UI

--- src/App/layouts/MainLayout/index.jsx
+++ src/App/layouts/MainLayout/index.jsx
@@ -1,6 +1,8 @@
 import { UserRoleEnum } from '@/App/constants/userRoles';
 import LoadingProgressBar from '@/Core/components/common/Loading/LoadingProgressBar';
-import { Fragment, Suspense, memo, useMemo, useState } from 'react';
+import UnsupportScreen from '@/Core/components/customs/UnsupportScreen';
+import ErrorBoundary from '@/Core/components/error/ErrorBoundary';
+import { Fragment, Suspense, useMemo, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { Outlet } from 'react-router-dom';
 import tw from 'twin.macro';
@@ -8,15 +10,11 @@ import DesktopSidebar from './components/DesktopSidebar';
 import MobileSidebar from './components/MobileSidebar';
 import Navbar from './components/Navbar';
 import {
+	adminNavigation,
 	managerNavigation,
 	staffNavigation,
-	studentNavigation,
-	adminNavigation
+	studentNavigation
 } from './constants/navigationConstants';
-import UnsupportScreen from '@/Core/components/customs/UnsupportScreen';
-import ErrorBoundary from '@/Core/components/error/ErrorBoundary';
-
-const Page = memo(Outlet);
 
 const MainLayout = () => {
 	const [sidebarOpen, setSidebarOpen] = useState(false);
@@ -47,24 +45,27 @@ const MainLayout = () => {
 			<Main>
 				<Navbar onToggleSidebar={setSidebarOpen} navigation={navigation} />
 				{/* Page content */}
-				<OutletWrapper>
-					<ErrorBoundary>
+				<Main.Content>
+					<Wrapper>
 						<Suspense fallback={<LoadingProgressBar />}>
-							<Page />
+							<ErrorBoundary>
+								<Outlet />
+							</ErrorBoundary>
 						</Suspense>
-					</ErrorBoundary>
-				</OutletWrapper>
+					</Wrapper>
+				</Main.Content>
 			</Main>
 		</Fragment>
 	);
 };
 
 const Main = tw.div`lg:pl-72 h-screen flex flex-col overflow-hidden`;
-const OutletWrapper = tw.div`py-10 px-4 sm:px-6 lg:px-8 h-full overflow-y-auto scrollbar-thin scrollbar-track-gray-50 scrollbar-thumb-gray-200`;
+Main.Content = tw.div`py-10 px-4 sm:px-6 lg:px-8 h-full overflow-y-auto scrollbar-thin scrollbar-track-gray-50 scrollbar-thumb-gray-200`;
 const SidebarContent = tw.div`flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6 py-4`;
-const Image = (props) => <img {...props} tw='max-w-[10rem] object-contain translate-x-2' />;
+const Image = tw.img`max-w-[10rem] object-contain translate-x-2`;
 const SidebarWrapper = tw.div`hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-72 lg:flex-col`;
 const Navigation = tw.nav`flex flex-1 flex-col`;
+const Wrapper = tw.div`min-h-fit`;
 
 export { Image, SidebarContent, SidebarWrapper, Navigation };
 export default MainLayout;
